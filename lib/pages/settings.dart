ddff5ca7085cc373831afffaf331a8a2c1bd2a67
Tyrael1999 <jamch1999@gmail.com>
2023-06-16 02:21:26 -0400

    template of upload image

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,9 +1,28 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter_fast_forms/flutter_fast_forms.dart';
 import '../widgets/custom_fast_date_range_picker.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
   final formKey = GlobalKey<FormState>();
+  File? selectedImage;
+
+  Future<void> pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        selectedImage = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -42,9 +61,15 @@ class SettingsPage extends StatelessWidget {
         ),
         Text('Agregar imagenes'),
         ElevatedButton.icon(
-          onPressed: () {},
+          onPressed: pickImage,
           icon: Icon(Icons.upload),
           label: Text('Agregar'),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 16.0),
+          child: selectedImage != null
+              ? Image.file(selectedImage!)
+              : Text('No se ha seleccionado una imagen'),
         ),
       ],
     );
