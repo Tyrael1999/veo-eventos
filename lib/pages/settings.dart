d39c218e1f689344276f8c0f98952d945bd5042b
Tyrael1999 <jamch1999@gmail.com>
2023-06-16 01:56:00 -0400

    create template of form of crear evento

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fast_forms/flutter_fast_forms.dart';
+import '../widgets/custom_fast_date_range_picker.dart';
 
 class SettingsPage extends StatelessWidget {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Settings Page'),
+    return Column(
+      children: [
+        Text('Crear Evento'),
+        FastForm(
+          formKey: formKey,
+          children: [
+            FastTextField(
+              name: 'nombre_evento',
+              labelText: 'Nombre del evento',
+            ),
+            FastTextField(
+              name: 'nombre_organizacion',
+              labelText: 'Agrupación organizadora',
+            ),
+            FastTextField(
+              name: 'descripcion_evento',
+              labelText: 'Descripción',
+            ),
+            FastTextField(
+              name: 'ubicacion_evento',
+              labelText: 'Ubicación',
+            ),
+            CustomFastDateRangePicker(
+              name: 'hora_evento',
+              labelText: 'Inicio - Termino',
+              firstDate: DateTime.now(),
+              lastDate: DateTime.now().add(const Duration(days: 365)),
+              cancelText: 'Cancelar',
+              fieldStartLabelText: 'Fecha inicio',
+              fieldEndLabelText: 'Fecha termino',
+              helpText: 'Escoge el rango de fechas del evento',
+            ),
+          ],
+        ),
+        Text('Agregar imagenes'),
+        ElevatedButton.icon(
+          onPressed: () {},
+          icon: Icon(Icons.upload),
+          label: Text('Agregar'),
+        ),
+      ],
     );
   }
 }
