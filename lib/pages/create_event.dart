69d24c554f6f17f912ae16913f2a274dd7512f1e
Tyrael1999 <jamch1999@gmail.com>
2023-06-23 14:17:27 -0400

    implementada vista formulario con ubicacion actual (provisional) y cambios menores

--- lib/pages/create_event.dart
+++ lib/pages/create_event.dart
@@ -5,6 +5,8 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:intl/intl.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class CreateEventPage extends StatefulWidget {
   @override
@@ -14,8 +16,10 @@ class CreateEventPage extends StatefulWidget {
 class _CreateEventPageState extends State<CreateEventPage> {
   final _formKey = GlobalKey<FormState>();
   Position? _currentPosition;
+  String? _currentAddress;
   final ImagePicker imgpicker = ImagePicker();
   List<XFile>? imagefiles;
+  TextEditingController _ubicacionController = TextEditingController();
 
   openImages() async {
     try {
@@ -49,17 +53,73 @@ class _CreateEventPageState extends State<CreateEventPage> {
     }
   }
 
-  _getCurrentLocation() {
-    Geolocator.getCurrentPosition(
-            desiredAccuracy: LocationAccuracy.best,
-            forceAndroidLocationManager: true)
-        .then((Position position) {
-      setState(() {
-        _currentPosition = position;
-      });
-    }).catchError((e) {
-      print(e);
-    });
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    _currentPosition = await Geolocator.getCurrentPosition();
+    _openMap(_currentPosition!.latitude, _currentPosition!.longitude);
+    _getAddressFromCoordinates();
+  }
+
+  Future<void> _openMap(double lat, double long) async {
+    print(long);
+    String googleUrl =
+        'https://www.google.com/maps/search/?api=1&query=$lat,$long';
+    await launchUrlString(googleUrl);
+  }
+
+  _getAddressFromCoordinates() async {
+    if (_currentPosition != null) {
+      try {
+        List<Placemark> placemarks = await placemarkFromCoordinates(
+          _currentPosition!.latitude,
+          _currentPosition!.longitude,
+        );
+        if (placemarks != null && placemarks.isNotEmpty) {
+          Placemark placemark = placemarks[0];
+          String formattedAddress =
+              '${placemark.street}, ${placemark.locality}';
+          setState(() {
+            _currentAddress = formattedAddress;
+          });
+        }
+
+        _ubicacionController.text = _currentAddress ?? '';
+      } catch (e) {
+        print(e);
+      }
+    }
   }
 
   @override
@@ -85,20 +145,28 @@ class _CreateEventPageState extends State<CreateEventPage> {
                   name: 'descripcion',
                   decoration: const InputDecoration(labelText: 'Descripción'),
                 ),
-                ElevatedButton.icon(
-                  onPressed: _getCurrentLocation,
-                  icon: Icon(Icons.location_on),
-                  label: Text(''),
+                FormBuilderTextField(
+                  name: 'ubicacion',
+                  onTap: () {
+                    _determinePosition();
+                  },
+                  controller: _ubicacionController,
+                  readOnly: true,
+                  decoration: InputDecoration(
+                      labelText: 'Ubicación',
+                      suffixIcon: Icon(Icons.location_on)),
                 ),
-                if (_currentPosition != null)
-                  Text(
-                      "LAT: ${_currentPosition?.latitude}, LNG: ${_currentPosition?.longitude}"),
                 FormBuilderDateTimePicker(
-                    name: 'date_established',
-                    format: DateFormat('dd/MM/yyyy'),
-                    enabled: true,
+                    name: 'fecha_inicio',
+                    format: DateFormat('dd/MM/yyyy HH:mm'),
                     decoration: InputDecoration(
-                      labelText: 'Fecha',
+                      labelText: 'Fecha inicio',
+                    )),
+                FormBuilderDateTimePicker(
+                    name: 'fecha_termino',
+                    format: DateFormat('dd/MM/yyyy HH:mm'),
+                    decoration: InputDecoration(
+                      labelText: 'Fecha termino (opcional)',
                     )),
               ],
             )),
