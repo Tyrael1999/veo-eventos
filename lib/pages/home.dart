c2ac17c70ef519bb57d2bb5b46f463027cad522c
Mecho-sys <nrojazamorano@hotmail.com>
2023-07-20 23:31:33 -0400

    2da version de la vista tipo tinder

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:veo_eventos/app_state.dart';
 import 'package:provider/provider.dart';
 
@@ -15,10 +16,45 @@ class HomePage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
+    List<Color> cardColors = [
+      Colors.red,
+      Colors.blue,
+      Colors.green,
+      Colors.yellow,
+      Colors.purple,
+    ]; // Tu lista de colores para el swiper
+
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          SizedBox(height: 10),
+          CarouselSlider.builder(
+            itemCount: cardColors.length,
+            itemBuilder: (BuildContext context, int index, int realIndex) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: cardColors[
+                      index], // Usamos el color de la lista en función del índice
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                child: Center(
+                  child: Text(
+                    'Tarjeta ${index + 1}',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                ),
+              );
+            },
+            options: CarouselOptions(
+              height: 200, // Altura del swiper
+              enlargeCenterPage: true,
+              enableInfiniteScroll: false,
+              initialPage: 0,
+              viewportFraction:
+                  0.6, // Tamaño de la tarjeta visible en la pantalla
+            ),
+          ),
           SizedBox(height: 10),
           Row(
             mainAxisSize: MainAxisSize.min,
